8cfed27de60971cf47cc16031971805fbd242de9
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2025-02-05 18:06:54 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 terraform {
-  #required_version = ">= v1.1.9"
+  required_version = ">= 1.5.5"
 
   required_providers {
     aws = {
-      # source                = "hashicorp/aws"
-      # version               = ">= 4.9"
+       source                = "hashicorp/aws"
+       version               = ">= 4.9"
       # configuration_aliases = [aws.lambda_region]
     }
   }
